4d49434ecf0c5b037777a82609de8617b262df63
volyx <volyihin@gmail.com>
2021-03-23 10:36:49 +0300

    use heroku port

--- src/main/java/io/github/volyx/initials/EmbeddingJettyWithServlet.java
+++ src/main/java/io/github/volyx/initials/EmbeddingJettyWithServlet.java
@@ -5,10 +5,18 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import java.util.Map;
+
 public class EmbeddingJettyWithServlet {
     public static void main(String[] args) throws Exception {
 
-        Server server = new Server(8080);
+        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+
+        final int port = Integer.parseInt(System.getenv("PORT"));
+
+        Server server = new Server(port);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
--- src/main/java/io/github/volyx/initials/InitialsAvatarsServlet.java
+++ src/main/java/io/github/volyx/initials/InitialsAvatarsServlet.java
@@ -13,7 +13,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
